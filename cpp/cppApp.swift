2987d08af0213204299f1f06c8ae0d7668e57f28
Jamil Nawaz <jamilnawaz88@gmail.com>
2023-01-11 23:56:24 +0000

    Initial Commit

---
+++ cpp/cppApp.swift
@@ -0,0 +1,17 @@
+//
+//  cppApp.swift
+//  cpp
+//
+//  Created by Jamil Nawaz on 11/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct cppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
